3e0cfd49374b3bf1f6d22ea6ef9fd33748b2dcbd
Phil Datoon <phil.datoon@yahoo.com>
2013-09-06 23:25:12 +0800

    String concatenation

---
+++ AT1/ConsoleApplication1/ConsoleApplication1/Act 01.cs
@@ -0,0 +1,25 @@
+﻿// Datoon, Philip Bryan B.
+// 22 July 2013
+// 131311399
+// String concatenation
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello C Sharp!\n");
+            Console.WriteLine("Please enter your Name : \n");   // user input UserName
+            string UserName = Console.ReadLine();               // reads the UserName input value
+            Console.WriteLine("Hello " + UserName);             // String concatenation
+            Console.Read();                                     // pause program to read information
+        }
+    }
+}

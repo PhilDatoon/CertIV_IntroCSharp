22c036b93f2dd133f9e54be9dbc60e98532b10f7
Phil Datoon <phil.datoon@yahoo.com>
2013-09-06 23:25:33 +0800

    Multiple place markings and pass variables

---
+++ AT1/ConsoleApplication1/ConsoleApplication1/Act 03.cs
@@ -0,0 +1,27 @@
+﻿// Datoon, Philip Bryan B.
+// 22 July 2013
+// 131311399
+// Multiple place markings and pass variables
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your first name :");        // user input FirstName
+            string FirstName = Console.ReadLine();                      // reads FirstName value entered
+            Console.WriteLine("Please enter your last name :");         // user input LastName
+            string LastName = Console.ReadLine();                       // reads LastName value entered
+            Console.WriteLine("Hello {0}, {1}", FirstName, LastName);   // Multiple place markings and pass variables
+
+            Console.Read();                                             // pause program to read information
+        }
+    }
+}

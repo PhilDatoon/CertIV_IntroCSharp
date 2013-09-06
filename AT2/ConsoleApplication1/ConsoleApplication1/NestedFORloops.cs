dfddf1399cf8ccf5dd8fefa55f07f55431b29667
Phil Datoon <phil.datoon@yahoo.com>
2013-09-06 23:32:00 +0800

    Using interface loops

---
+++ AT2/ConsoleApplication1/ConsoleApplication1/NestedFORloops.cs
@@ -0,0 +1,32 @@
+﻿// Datoon, Philip Bryan B.
+// 26 July 2013
+// 131311399
+// Using interface loops
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j;
+
+            for (i = 2; i < 100; i++)
+            {
+                for (j = 2; j <= (i / j); j++)
+                    if ((i % j) == 0) break;                // if factor found, not a prime
+
+                if (j > (i / j))
+                    Console.WriteLine("{0} is prime.", i);  // displays if number is a prime
+            }
+
+            Console.Read();                                 // pause program to read information
+        }
+    }
+}

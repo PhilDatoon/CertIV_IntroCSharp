9eb774467799341ed11b9323681d416d3f7e0f72
Phil Datoon <phil.datoon@yahoo.com>
2013-09-06 23:25:43 +0800

    Using mathematical operators

---
+++ AT1/ConsoleApplication1/ConsoleApplication1/Act 04.cs
@@ -0,0 +1,31 @@
+﻿// Datoon, Philip Bryan B.
+// 22 July 2013
+// 131311399
+// Using mathematical operators
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Divide");
+            int Numerator = 11;                                     // declaration and initialisation of variables
+            int Denominator = 2;
+
+            int Result1 = Numerator / Denominator;                  // calculates the quotient
+            int Result2 = Numerator % Denominator;                  // calculates the remainder
+
+            Console.WriteLine("Quotient Result = {0}", Result1);    // displays the quotient
+            Console.WriteLine("Remainder Result = {0}", Result2);   // displays the remainder
+
+            Console.Read();                                         // pause program to read information
+        }
+    }
+}

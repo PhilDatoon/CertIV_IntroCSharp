4c12c81f17632a78cccb2d4066d85e8d6e3bbdf6
Phil Datoon <phil.datoon@yahoo.com>
2013-09-06 23:32:46 +0800

    Using sequential search algorithm within a single dimensional array

---
+++ AT2/ConsoleApplication1/ConsoleApplication1/SequentialSearch.cs
@@ -0,0 +1,42 @@
+﻿// Datoon, Philip Bryan B.
+// 31 July 2013
+// 131311399
+// Using sequential search algorithm within a single dimensional array
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arrayNum = { 10, 3, 9, 4, 8, 1, 7, 2, 6, 5 };
+
+            Console.Write("Enter a number (1 to 10): ");
+            int target = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            // Sequential search algorithm
+            for (int i = 0; i < arrayNum.Length; i++)
+            {
+                if (arrayNum[i] == target)
+                {
+                    Console.WriteLine(">> Number {0} is found. Located in array[{1}].", target, i);
+                    break;      // stops searching and exits FOR loop
+                }
+
+                else
+                {
+                    Console.WriteLine("...");
+                }
+            }
+
+            Console.Read();
+        }
+    }
+}

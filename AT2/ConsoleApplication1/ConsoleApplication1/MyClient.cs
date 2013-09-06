8a66b72176eb8c8a1224b245ac0af231d64d570b
Phil Datoon <phil.datoon@yahoo.com>
2013-09-06 23:31:54 +0800

    Using exception handling in dividing a number by zero

---
+++ AT2/ConsoleApplication1/ConsoleApplication1/MyClient.cs
@@ -0,0 +1,46 @@
+﻿// Datoon, Philip Bryan B.
+// 29 July 2013
+// 131311399
+// Using exception handling in dividing a number by zero
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class MyClient
+    {
+        static void Main(string[] args)
+        {
+            int x = 0;
+            int div = 0;
+
+            try
+            {
+                div = 100 / x;
+                Console.WriteLine("Not executed.");
+            }
+            // catches exceptions on dividing a number by zero
+            catch (DivideByZeroException de)
+            {
+                Console.WriteLine("DivideByZeroException.");
+            }
+            // catches exceptions on invalid data type input
+            catch (Exception ee)
+            {
+                Console.WriteLine("oException");
+            }
+
+            finally
+            {
+                Console.WriteLine("Finally block.");
+            }
+            
+            Console.WriteLine("Result is {0}.", div);
+            Console.Read();
+        }
+    }
+}

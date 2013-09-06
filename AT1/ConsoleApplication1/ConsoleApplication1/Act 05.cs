080c1fa62b5d2e1a6db9c2740a46c6e000d0e190
Phil Datoon <phil.datoon@yahoo.com>
2013-09-06 23:25:51 +0800

    Using IF-ELSE statement

---
+++ AT1/ConsoleApplication1/ConsoleApplication1/Act 05.cs
@@ -0,0 +1,39 @@
+﻿// Datoon, Philip Bryan B.
+// 22 July 2013
+// 131311399
+// Using IF-ELSE statement
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("is equal \n");
+            Console.Write("Enter a number: ");              // User input Number
+            int Number = int.Parse(Console.ReadLine());     // reads Number value entered
+            
+            // Start of IF-ELSE statement
+            // performs if number is greater than 10
+            if (Number > 10)
+            {
+                Console.WriteLine("The variable number is greater than 10.");
+            }
+            // performs if number is lesser than 10
+            else
+            {
+                Console.WriteLine("The variable number is lesser than 10.");
+            }
+            // End of IF-ELSE statement
+
+            // pause program to read information
+            Console.Read();
+        }
+    }
+}

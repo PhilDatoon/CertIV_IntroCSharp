3ab790db7d668404745ed41132391f74c6a21859
Phil Datoon <phil.datoon@yahoo.com>
2013-09-06 23:28:08 +0800

    Ways of declaring and initialising array

---
+++ AT1/ConsoleApplication1/ConsoleApplication1/Act 18.cs
@@ -0,0 +1,42 @@
+﻿// Datoon, Philip Bryan B.
+// 29 July 2013
+// 131311399
+// Different ways of declaring and initialising array
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int max = 3;
+            int[] EvenNumbers = new int[3];         // declaration of array EvenNumbers
+            int[] OddNumbers = {3, 5, 7};           // declaration and initialising array OddNumbers
+
+            EvenNumbers[0] = 2;                     // initialising EvenNumbers[] elements
+            EvenNumbers[1] = 4;
+            EvenNumbers[2] = 6;
+
+            // displays all elements of EvenNumbers[]
+            for (int j = 0; j < max; j++)
+            {
+                Console.WriteLine(EvenNumbers[j]);
+            }
+
+            // displays all elements of OddNumbers[]
+            for (int k = 0; k < max; k++)
+            {
+                Console.WriteLine(OddNumbers[k]);
+            }
+
+            // pauses program to read information
+            Console.Read();
+        }
+    }
+}

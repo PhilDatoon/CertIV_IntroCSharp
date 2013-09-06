e23b7a308cf27ebc695d73ccb36b35261fd6fa7c
Phil Datoon <phil.datoon@yahoo.com>
2013-09-06 23:30:58 +0800

    Defining constants

---
+++ AT2/ConsoleApplication1/ConsoleApplication1/DeclaringConstants.cs
@@ -0,0 +1,28 @@
+﻿// Datoon, Philip Bryan B.
+// 26 July 2013
+// 131311399
+// Defining constants
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const double pi = 3.14159;                  // declaring constant variable
+            double r;
+            Console.WriteLine("Enter radius: ");
+            r = Convert.ToDouble(Console.ReadLine());   // converts input to double data type
+            double areaCircle = pi * r * r;             // calculating areaCircle
+            Console.WriteLine("Radius: {0}, Area: {1}", r, areaCircle);
+
+            Console.Read();
+        }
+    }
+}
